8a8f5334d0156526b6fe39acb68a0d8ca597b7e9
Md Harun Or Rashid <horrashid007@gmail.com>
2020-03-30 13:04:48 +0600

    Testing Recipe API

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,19 @@
-import str from './models/Search';
-// import { add as a, multiply as m } from './views/searchView';
-import * as searchView from './views/searchView';
+import axios from 'axios';
 
-console.log(
-  `Using imported functions ! ${searchView.add(3, 3)} and ${searchView.multiply(
-    2,
-    3
-  )}. ${str}`
-);
+// "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
+
+async function getResults(query) {
+  const key = '52f194fce6211fe426cd3832da9055d9';
+  const id = '02410039';
+  try {
+    const res = await axios(
+      `https://api.edamam.com/search?q=${query}&app_id=${id}&app_key=${key}&from=0&to=30`
+    );
+    const recipes = res.data.hits;
+    console.log(recipes);
+  } catch (err) {
+    alert(err);
+  }
+}
+
+getResults('pizza');
