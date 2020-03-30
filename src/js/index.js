59b8c2a54636e18a5664d704227737cd062367f3
Md Harun Or Rashid <horrashid007@gmail.com>
2020-03-30 12:04:27 +0600

    ES6 module import export

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-// Global app controller
+import str from './models/Search';
+// import { add as a, multiply as m } from './views/searchView';
+import * as searchView from './views/searchView';
+
+console.log(
+  `Using imported functions ! ${searchView.add(3, 3)} and ${searchView.multiply(
+    2,
+    3
+  )}. ${str}`
+);
---
+++ src/js/models/Search.js
@@ -0,0 +1,1 @@
+export default 'I am exported string.';
