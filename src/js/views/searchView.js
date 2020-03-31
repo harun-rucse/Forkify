6e2cc38622bfeb035da927f08b07f93db672f7f2
Md Harun Or Rashid <horrashid007@gmail.com>
2020-03-31 11:47:28 +0600

    limit the recipe title of length 17 without cutting word

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,16 +8,41 @@ export const clearInput = () => {
 export const clearResult = () => {
   elements.searchResList.innerHTML = '';
 };
+/**
+ * Title: Pasta with tomato and spinach
+ * initial accumulator is 0
+ * acc: 0 / acc + cur.length: 5/ newTitle = ['Pasta']
+ * acc: 5 / acc + cur.length: 9/ newTitle = ['Pasta', 'with']
+ * acc: 9 / acc + cur.length: 15/ newTitle = ['Pasta', 'with', 'tomato']
+ * acc: 15 / acc + cur.length: 18/ newTitle = ['Pasta', 'with', 'tomato']
+ */
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = [];
+  if (title.length > limit) {
+    title.split(' ').reduce((acc, cur) => {
+      if (acc + cur.length <= limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    // return the result
+    return `${newTitle.join(' ')} ...`;
+  }
+};
 
 const renderRecipie = recipe => {
   const markup = `
         <li>
             <a class="results__link" href="#23456">
                 <figure class="results__fig">
-                    <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+                    <img src="${recipe.recipe.image}" alt="${
+    recipe.recipe.label
+  }">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.recipe.label}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(
+                      recipe.recipe.label
+                    )}</h4>
                     <p class="results__author">${recipe.recipe.source}</p>
                 </div>
             </a>
